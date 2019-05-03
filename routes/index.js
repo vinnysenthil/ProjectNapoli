af2e8928e6ea2cc7750abd5bf299b822d43f61fe
Vinny <vinnysenthil@gmail.com>
2019-05-03 15:31:11 -0700

    Add Okta default messages endpoint to /api

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const sequelize = require('../services/sequelize');
 var router = express.Router();
+var cors = require('cors');
 
 // Test endpoint
 router.get('/', function(req, res, next) {
@@ -12,6 +13,26 @@ router.get('/depts', async function(req, res, next) {
   var allDepts = await sequelize.Departments.findAll()
   res.status(200).send(allDepts);
 });
+
+/**
+ * Another example route that requires a valid access token for authentication, and
+ * print some messages for the user if they are authenticated
+ */
+router.get('/messages', (req, res) => {
+  res.json({
+    messages: [
+      {
+        date:  new Date(),
+        text: 'I am a robot.'
+      },
+      {
+        date:  new Date(new Date().getTime() - 1000 * 60 * 60),
+        text: 'Hello, world!'
+      }
+    ]
+  });
+});
+
 
 // Load other routes
 router.use('/employees', require('./employees'));
