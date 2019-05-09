5975e5de82838b3c44fca9a7bee062d7f2e9d690
Vinny <vinnysenthil@gmail.com>
2019-05-09 01:38:27 -0700

    Whitelisted redirect_uri and made it absolute

--- front_end/src/.samples.config.js
+++ front_end/src/.samples.config.js
@@ -2,10 +2,10 @@ export default {
   oidc: {
     clientId: "0oaklp0zjhYuM9yoE0h7",
     issuer: "https://dev-489491.oktapreview.com/oauth2/default",
-    redirectUri: `${window.location.origin}/implicit/callback`, /// ,aybe 8080???
+    redirectUri: `https://project-napoli.herokuapp.com/implicit/callback`, /// ,aybe 8080???
     scope: "openid profile email"
   },
   resourceServer: {
-    messagesUrl: `${window.location.origin}/api/messages`
+    messagesUrl: `${window.location.origin}:172/api/messages`
   }
 };
