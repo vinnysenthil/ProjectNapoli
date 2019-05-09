08d479fda77a5549e24dd4dd8170683add85bbe1
Vinny <vinnysenthil@gmail.com>
2019-05-09 02:59:32 -0700

    Dynamic redirectUri for Okta

--- front_end/src/.samples.config.js
+++ front_end/src/.samples.config.js
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: "0oaklp0zjhYuM9yoE0h7",
     issuer: "https://dev-489491.oktapreview.com/oauth2/default",
-    redirectUri: `https://project-napoli.herokuapp.com/implicit/callback`, /// ,aybe 8080???
+    redirectUri: `${window.location.origin}/implicit/callback`,
     scope: "openid profile email"
   },
   resourceServer: {
